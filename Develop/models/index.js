4695c290efa0f20bcd0d6102769d79aeb72ddf91
walker <spadonival7@gmail.com>
2024-06-09 12:53:13 -0400

    completed routes so that they can perform create,read,update, and delete operations using my sequelize models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  as: 'products',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id',
+    unique: false,
+  },
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  unique: false,
+});
 module.exports = {
   Product,
   Category,
